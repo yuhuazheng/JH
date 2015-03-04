b6622392663d1d6862f674cc70e1efd169195d3b
yuhuazheng <yuhuazheng2006@gmail.com>
2015-03-04 10:52:01 -0800

    leetcode clone graph

---
+++ src/Algorithms/CloneGraphDepthFirst.java
@@ -0,0 +1,42 @@
+package myPractice;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class CloneGraphDepthFirst {
+
+	/*--
+	 * use stack to keep unvisited node
+	 * use hashmap to keep copied node
+	 */
+	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+		//boundary condition
+		if(node==null){
+			return null;
+		}
+				
+		//copied to ensure all nodes get a copy
+		HashMap<Integer,UndirectedGraphNode> copied = new HashMap<Integer,UndirectedGraphNode>();
+		UndirectedGraphNode copy = new UndirectedGraphNode(node.label);
+		copied.put(node.label, copy);
+		//stack only adds node when a copy is created; so that all node will be visited once
+		Stack<UndirectedGraphNode> toVisit = new Stack<UndirectedGraphNode>();
+		toVisit.push(node);
+		
+		//start traverse
+		while(!toVisit.isEmpty()){
+			UndirectedGraphNode origin = toVisit.pop();
+			copy = copied.get(origin.label);
+			//check neighbors
+			for(UndirectedGraphNode curNeighbor : origin.neighbors){
+				if(!copied.containsKey(curNeighbor.label)){
+					UndirectedGraphNode copyNeighbor = new UndirectedGraphNode(curNeighbor.label);
+					copied.put(curNeighbor.label, copyNeighbor);
+					toVisit.push(curNeighbor);
+				}
+				copy.neighbors.add(copied.get(curNeighbor.label));
+			}
+		}
+		return copied.get(node.label);
+	}
+}
---
+++ src/Algorithms/CloneGraphWidthFirst.java
@@ -0,0 +1,43 @@
+package myPractice;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CloneGraphWidthFirst {
+
+	/*--
+	 * use queue to keep unvisited node
+	 * use hashmap to keep copied node
+	 */
+	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+		//boundary condition
+		if(node==null){
+			return null;
+		}
+				
+		//copied to ensure all nodes get a copy
+		HashMap<Integer,UndirectedGraphNode> copied = new HashMap<Integer,UndirectedGraphNode>();
+		UndirectedGraphNode copy = new UndirectedGraphNode(node.label);
+		copied.put(node.label, copy);
+		//stack only adds node when a copy is created; so that all node will be visited once
+		Queue<UndirectedGraphNode> toVisit = new LinkedList<UndirectedGraphNode>();
+		toVisit.offer(node);
+		
+		//start traverse
+		while(!toVisit.isEmpty()){
+			UndirectedGraphNode origin = toVisit.poll();
+			copy = copied.get(origin.label);
+			//check neighbors
+			for(UndirectedGraphNode curNeighbor : origin.neighbors){
+				if(!copied.containsKey(curNeighbor.label)){
+					UndirectedGraphNode copyNeighbor = new UndirectedGraphNode(curNeighbor.label);
+					copied.put(curNeighbor.label, copyNeighbor);
+					toVisit.offer(curNeighbor); //stack ensures depth-first
+				}
+				copy.neighbors.add(copied.get(curNeighbor.label));
+			}
+		}
+		return copied.get(node.label);
+	}
+}
