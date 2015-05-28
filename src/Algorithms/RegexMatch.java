6a2bfbbe426aa1be88373d09fe67f2c0847b9357
yuhuazheng <yuhuazheng2006@gmail.com>
2015-05-27 23:57:55 -0700

    regex match

---
+++ src/Algorithms/RegexMatch.java
@@ -0,0 +1,23 @@
+package Algorithms;
+
+public class RegexMatch {
+
+	public boolean isMatch(String s, String p) {
+		if(p.length()==0) return s.length()==0;
+		if(p.length()==1)
+			return ( (s.length()==1) && (p.charAt(0)=='.' || p.charAt(0)==s.charAt(0)) );
+		if(p.charAt(1)=='*'){
+			if(isMatch(s,p.substring(2))){
+				return true;
+			}
+			return ( s.length()>0
+					 && (p.charAt(0)=='.' || p.charAt(0)==s.charAt(0))
+					 && isMatch(s.substring(1),p));
+		}
+		else{
+			return ( s.length()>0
+					 && (p.charAt(0)=='.' || p.charAt(0)==s.charAt(0))
+					 && isMatch(s.substring(1),p.substring(1)));
+		}
+	}
+}
