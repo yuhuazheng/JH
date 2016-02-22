5c2bc01989551d80f96e9f89121e596544720ff1
yuhuazheng <yuhuazheng2006@gmail.com>
2016-02-21 18:41:14 -0800

    some more problems

--- src/Algorithms/OddEvenLL.java
+++ src/Algorithms/OddEvenLL.java
@@ -8,31 +8,19 @@ public class OddEvenLL {
     public ListNode oddEvenList(ListNode head) {
         if(head==null|| head.next==null || head.next.next==null) return head;
 
-        ListNode ohead = new ListNode(0);
-        ListNode ehead = new ListNode(0);
+        ListNode ohead = head;
+        ListNode ehead = head.next;
         ListNode ocur = ohead;
-        ListNode onext = head;
         ListNode ecur = ehead;
-        ListNode enext = head.next;
-        while(onext!=null&&enext!=null){
-            ocur.next=onext;
-            onext=enext==null?null:enext.next;
+
+        while(ecur!=null&&ecur.next!=null){
+            ocur.next=ecur.next;
             ocur=ocur.next;
 
-            ecur.next=enext;
-            enext=onext==null?null:onext.next;
+            ecur.next=ocur.next;
             ecur=ecur.next;
         }
-        if(onext!=null){
-            ocur.next=onext;
-            ocur=ocur.next;
-        }
-        ocur.next=ehead.next;
-        if(enext!=null){
-            ecur.next=enext;
-            ecur=ecur.next;
-        }
-        ecur.next=null;
-        return ohead.next;
+        ocur.next=ehead;
+        return ohead;
     }
 }
