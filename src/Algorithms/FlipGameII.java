9fb1f628161db0689995ebcfc9e581a382516ad6
yuhuazheng <yuhuazheng2006@gmail.com>
2016-02-21 16:57:04 -0800

    some more problems

--- src/Algorithms/FlipGameII.java
+++ src/Algorithms/FlipGameII.java
@@ -14,6 +14,7 @@ public class FlipGameII {
 
     public boolean canWin(String s) {
         for (int i = 0; i < s.length() - 1; ++i)
+            //such 2-player game is suitable for recursion: one's win is the other's loss
             if (s.charAt(i) == '+' && s.charAt(i + 1) == '+' && !canWin(s.substring(0, i) + "--" + s.substring(i + 2)))
                 return true;
         return false;
