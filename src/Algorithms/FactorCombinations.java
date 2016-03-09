d500ce7211e2db7b2078f1e6aacf5972b7afcac4
yuhuazheng <yuhuazheng2006@gmail.com>
2016-03-08 20:21:36 -0800

    some code exercises

--- src/Algorithms/FactorCombinations.java
+++ src/Algorithms/FactorCombinations.java
@@ -19,40 +19,27 @@ public class FactorCombinations {
 
     public List<List<Integer>> getFactors(int n) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(n<2) return res;
 
-        List<Integer> item = new ArrayList<Integer>();
-        helper(n, item, res);
+        helper(n,2,new ArrayList<Integer>(),res);
         return res;
     }
 
-    private void helper(int n, List<Integer> item, List<List<Integer>> res){
+    private void helper(int n, int start, ArrayList<Integer> cur, List<List<Integer>> res){
         if(n==1){
-            return;
+            if(cur.size()>1) //to rule out the original number itself
+                res.add(new ArrayList<Integer>(cur));
         }
 
-        int divider=item.size()==0?2:item.get(item.size()-1); //keep increase order of factors, ensure no dup
-        int remainer=n;
-        while(divider<=n/2){
-            if(n%divider!=0){
-                divider++;
+        for(int i=start;i<=n;i++){
+            if(n%i!=0){
                 continue;
             }
-            remainer = n/divider;
-            if(divider>remainer){
-                break;
+            else{
+                cur.add(i);
+                helper(n/i,i,cur,res);
+                cur.remove(cur.size()-1);
             }
-            //add current combine into res
-            item.add(divider);
-            item.add(remainer);
-            res.add(new ArrayList<Integer>(item));
-
-            //backtracking remainer and break down the remainer
-            item.remove(item.size()-1);
-            helper(remainer, item, res);
-
-            //backtracking divider
-            item.remove(item.size()-1);
-            divider++;
         }
     }
 }
