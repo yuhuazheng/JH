fddea983773de07db46cd607c8cc2d9c36090c16
yuhuazheng <yuhuazheng2006@gmail.com>
2016-03-09 23:56:06 -0800

    some more problems

--- src/Algorithms/FindPeakElement.java
+++ src/Algorithms/FindPeakElement.java
@@ -15,45 +15,20 @@ public class FindPeakElement {
 		if(num.length==1){
 			return 0;
 		}
-		return helper(num,0,num.length-1);
-	}
-	
-	private int helper(int[] num, int l, int r){
-		if(r<l){
-			return -1;
+		int l=0;
+		int r=num.length-1;
+		int m=0;
+		while(l<=r){
+			m = (l+r)/2;
+			if( (m==0||num[m]>num[m-1])
+				&&(m==num.length-1||num[m]>num[m+1])
+					)
+				return m;
+			else if(m>0 && num[m]<num[m-1]) //m-1>m, left half must has a peak
+				r=m-1;				 //it either bigger than its neighbor, or be m[0].
+			else
+				l=m+1;
 		}
-		
-		int m=(l+r)/2;
-		// m and m+1 will be the test targets
-		// given num[-1] and num[n] as special case, we should treat m with 3 cases:
-		// left end, right end, and middle.
-		//left end, either found, or go to the right
-		//right end, either found, or go to the left
-		//middle, also need to check boundary, and then recursive.
-		if(m==0){
-			if(num[m]>num[m+1]){
-				return 0;
-			}
-			return helper(num,m+1,r);
-		}
-		else if(m==num.length-1){
-			if(num[m]>num[m-1]){
-				return num.length-1;
-			}
-			return helper(num,l,m-1);
-		}
-		else{
-			if(num[m]<num[m+1]){
-				if( (m+2>num.length-1) || (m+2<num.length && num[m+1]>num[m+2])){
-					return m+1;
-				}
-			}
-			else{
-				if( (m-1<0) || (m-1>=0 && num[m]>num[m-1]) ){
-					return m;
-				}
-			}
-			return Math.max(helper(num,m+1,r), helper(num,l,m-1));
-		}
+		return m;
 	}
 }
