fa8b9b986b6d1a0afe5aa9c278f6d6a471a3b4d2
yuhua <yuhuazheng2006@gmail.com>
2016-07-07 23:18:02 -0700

    remove test

---
+++ test/DummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by yuhua on 05/07/16.
+ */
+public class DummyTest {
+    @Test
+    public void testNothing() {
+        Assert.assertTrue(true);
+    }
+}
