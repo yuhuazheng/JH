a0b3e55cbe106db3ab514ec9425adc3b78dde575
yuhua <yuhuazheng2006@gmail.com>
2016-07-18 22:40:30 -0700

    remove test

--- src/Algorithms/PalindromeNumber.java
+++ src/Algorithms/PalindromeNumber.java
@@ -1,24 +1,25 @@
-package Algorithms;
 
 public class PalindromeNumber {
 
 	public  boolean isPalindrome(int x) {
-		if(x<0){
-			return false;
-		}
-		int div=1;
-		while(x>=div*10){
-			div*=10;
-		}
-		while(x>=0){
-			int low = x%10;
-			int high = x/div;
-			if(high!=low){
-				return false;
-			}
-			x=(x%div)/10;
-			div/=100;
+		if(x<0) return false;
+		if(x>=0&&x<10) return true;
+		int d=1;
+		while(d<=x/10)
+			d*=10;
+		while(x>0){
+			int v1=x/d;
+			int v2=x%10;
+			if(v1!=v2) return false;
+			x%=d;
+			x/=10;
+			d/=100;
 		}
 		return true;
+	}
+
+	public static void main(String[] args) {
+		PalindromeNumber inst = new PalindromeNumber();
+		System.out.println(inst.isPalindrome(1410110141));
 	}
 }
