dbd6da7ebb9e9c757e9827f106a2ccef4383d749
yuhuazheng <yuhuazheng2006@gmail.com>
2016-08-07 16:49:39 -0700

    leetcode

--- src/Algorithms/SearchRange.java
+++ src/Algorithms/SearchRange.java
@@ -1,4 +1,3 @@
-package myPractice;
 
 public class SearchRange {
 
@@ -11,47 +10,37 @@ public class SearchRange {
 		System.out.println(range[1]);
 	}
 	
-	public int[] searchRange(int[] A, int target){
-		int[] res = {-1,-1};
-	    if(A==null || A.length==0)
-	    {
-	        return res;
-	    }
-	    int ll = 0;
-	    int lr = A.length-1;
-	    while(ll<=lr)
-	    {
-	        int m = (ll+lr)/2;
-	        if(A[m]<target)
-	        {
-	            ll = m+1;
-	        }
-	        else
-	        {
-	            lr = m-1;
-	        }
-	    }
-	    
-	    int rl = 0;
-	    int rr = A.length-1;
-	    while(rl<=rr)
-	    {
-	        int m = (rl+rr)/2;
-	        if(A[m]<=target)
-	        {
-	            rl = m+1;
-	        }
-	        else
-	        {
-	            rr = m-1;
-	        }
-	    }
-	    
-	    if(ll<=rr)
-	    {
-	        res[0] = ll;
-	        res[1] = rr;
-	    }
-	    return res;
+	public int[] searchRange(int[] nums, int target){
+		int[] res = new int[2];
+		if(nums==null||nums.length==0){
+			res[0]=res[1]=-1;
+			return res;
+		}
+
+		int l=0;
+		int r=nums.length-1;
+		while(l<=r){
+			int m= (l+r)/2;
+			int v = nums[m];
+			if(v<target)
+				l=m+1;
+			else
+				r=m-1;
+		}
+		res[0]=(l<nums.length&&nums[l]==target)?l:-1;
+
+		l=0;
+		r=nums.length-1;
+		while(l<=r){
+			int m= (l+r)/2;
+			int v = nums[m];
+			if(v<=target)
+				l=m+1;
+			else
+				r=m-1;
+		}
+		res[1]=(r>=0&&nums[r]==target)?r:-1;
+
+		return res;
 	}
 }
