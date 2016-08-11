51065713328b79aafde3d49ff176f44f20a91957
yuhuazheng <yuhuazheng2006@gmail.com>
2016-08-11 14:58:42 -0700

    leetcode

--- src/Algorithms/Search2DMatrixII.java
+++ src/Algorithms/Search2DMatrixII.java
@@ -1,4 +1,3 @@
-package Algorithms;
 
 import java.util.ArrayList;
 
@@ -9,31 +8,40 @@ public class Search2DMatrixII {
 
     public static void main(String[] args){
         int[][] matrix = {
-                {1,   4,  7, 11, 15},
-                {2,   5,  8, 12, 19},
-                {3,   6,  9, 16, 22},
-                {10, 13, 14, 17, 24},
-                {18, 21, 23, 26, 30}
+                {-1,  3}
         };
         Search2DMatrixII inst = new Search2DMatrixII();
-        System.out.println(inst.searchMatrix(matrix,5));
+        System.out.println(inst.searchMatrix(matrix,3));
     }
 
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix==null ||matrix.length==0||matrix[0].length==0) return false;
-        int row = matrix.length;
-        int col = matrix[0].length;
+        if(matrix==null||matrix.length==0||matrix[0].length==0) return false;
+        if(matrix.length==1&&matrix[0].length==1) return matrix[0][0]==target;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        return dnc(0,m-1,0,n-1,matrix,target);
+    }
 
-        int r=0;
-        int c=col-1;
-        //from top-right, shrink
-        while(r<row&&c>=0){
-            int x = matrix[r][c];
-            if(x==target) return true;
-            if(x>target) c--; //this col is not valid
-            else r++; //this row is not valid
+    private boolean dnc(int top, int botm, int l, int r, int[][] matrix, int target){
+        if(top>botm||l>r) return false;
+
+        int midrow=(top+botm)/2;
+        int midcol=(l+r)/2;
+        int v = matrix[midrow][midcol];
+
+        if(v==target)
+            return true;
+        else if(v>target){
+            return (dnc(top,midrow-1,l,midcol-1,matrix,target)
+                    ||dnc(top,midrow-1,midcol,r,matrix,target)
+                    ||dnc(midrow,botm,l,midcol-1,matrix,target)
+            );
         }
-        return false;
-
+        else{
+            return (dnc(midrow+1,botm,midcol+1,r,matrix,target)
+                    ||dnc(top,midrow,midcol+1,r,matrix,target)
+                    ||dnc(midrow+1,botm,l,midcol,matrix,target)
+            );
+        }
     }
 }
