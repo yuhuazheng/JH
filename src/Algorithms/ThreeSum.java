2281a80a3a2f014f8c86e08547c768ec4dc8fc7d
yuhua <yuhuazheng2006@gmail.com>
2016-09-23 01:16:07 -0700

    just more coding practise

--- src/Algorithms/ThreeSum.java
+++ src/Algorithms/ThreeSum.java
@@ -1,4 +1,3 @@
-package myPractice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,7 +13,47 @@ public class ThreeSum {
 			System.out.println(oneRes);
 		}
 	}
-	
+
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		if(nums==null||nums.length<3)
+			return res;
+
+		Arrays.sort(nums);
+		for(int i=0;i<nums.length-2;i++){
+			if(i>0&&nums[i]==nums[i-1])
+				continue;
+
+			int l=i+1;
+			int r=nums.length-1;
+			int target = -nums[i];
+			while(l<r){
+				int sum = nums[l]+nums[r];
+				if(sum>target){
+					r--;
+				}
+				else if(sum<target){
+					l++;
+				}
+				else{
+					List<Integer> temp = new ArrayList<>();
+					temp.add(nums[l]);
+					temp.add(nums[r]);
+					temp.add(nums[i]);
+					res.add(temp);
+					l++;
+					r--;
+					while(l<r&&nums[l]==nums[l-1])
+						l++;
+					while(l<r&&nums[r]==nums[r+1])
+						r--;
+				}
+			}
+		}
+		return res;
+	}
+
+	/*
 	public List<List<Integer>> threeSum(int[] num) {
 		List<List<Integer>> results = new ArrayList<List<Integer>>();
 		
@@ -71,4 +110,5 @@ public class ThreeSum {
 		}
 		return results2;
 	}
+	*/
 }
